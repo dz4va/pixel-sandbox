196ce1baf0e63016608bff782755b52bf1585679
George Dzavashvili <dzavashviligeorge@gmail.com>
2021-11-20 00:07:35 +0400

    Add sandbox automata initial code

---
+++ pixel-sandbox/Assets/Source/SandboxAutomata.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum Direction {
+    Down, DownRight, DownLeft,
+    Up, UpRight, UpLeft,
+    Left, Right
+}
+
+public class SandboxAutomata : MonoBehaviour
+{
+    [SerializeField]
+    Texture2D sandboxTexture;
+    [SerializeField]
+    float simulationSpeed;
+    [SerializeField]
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
